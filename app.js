c5847b15e5a1412ff12c15170628f252031c670e
Phuong Hoang <hoangphuong965@gmail.com>
2020-10-01 10:56:05 +0700

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
-const btn = document.querySelector("button");
-const popup = document.querySelector(".popup-wrapper");
-const popupclose = document.querySelector(".popup-close");
+const clock = () => {
+  var hours, mins, secs;
+  const fullDate = new Date();
+  hours = fullDate.getHours();
+  mins = fullDate.getMinutes();
+  secs = fullDate.getSeconds();
 
-btn.addEventListener("click", () => {
-  popup.style.display = "block";
-});
-popupclose.addEventListener("click", () => {
-  popup.style.display = "none";
-});
+  hours < 10 ? (hours = "0" + hours) : hours;
+  mins < 10 ? (mins = ":0" + mins) : (mins = ":" + mins);
+  secs < 10 ? (secs = ":0" + secs) : (secs = ":" + secs);
+
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = mins;
+  document.getElementById("second").innerHTML = secs;
+};
+setInterval(clock, 1000);
+const alerts = () => {
+  const msg = document.querySelector(".msg");
+  msg.style.display = "block";
+  setTimeout(() => {
+    msg.style.display = "none";
+  }, 500);
+};
+
+const time = () => {
+  let min = document.getElementById("set_min");
+  if (min.value === "") {
+    alert("Enter Your Minutes");
+  } else {
+    alerts();
+    min = document.getElementById("set_min").value * 5000;
+    setTimeout(() => {
+      show();
+    }, min);
+  }
+};
+
+const show = () => {
+  let show = document.querySelector(".wrap");
+  const audio = document.getElementById("myAudio");
+
+  show.style.display = "block";
+  audio.play();
+  setTimeout(() => {
+    audio.pause();
+    show.style.display = "none";
+  }, 16000);
+};
